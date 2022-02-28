307bd852f58cb08fc60417f8049cd728966c6503
june0-K <ts1revan@naver.com>
2022-02-28 17:26:31 +0900

    update index, Faq, Guide

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Route } from "react-router-dom" 
 import Header from "./common/Header";
 import Footer from "./common/Footer";
+import Faq from "./faq/Faq";
+import Guide from "./guide/Guide";
 const Root = () => {
   return (
     <div>
       <Header />
-      <div className="inner_box">
-        {/*  inner-box 에 있는 컴포넌트 추가하시면 됩니다.  */}
+      <div className="root">
+        <Route exact path={"/"} component={Guide} />
+        <Route exact path={"/faq"} component={Faq} />
       </div>
       <Footer />
     </div>
