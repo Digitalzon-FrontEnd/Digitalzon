015c6a2fadb55c92cb5b1b997f72435dbc6fc69e
Jang Woo Bin <jwb449730@naver.com>
2022-02-28 14:26:29 +0900

    test

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./common/Header";
 import Footer from "./common/Footer";
+import Main from "./main/Main";
+import { Route, Router } from "react-router-dom";
+import Faq from "./faq/Faq";
+import Guide from "./guide/Guide";
+import Login from "./login/Login";
+import Terms from "./login/signup/Terms";
+import Panel from "./login/panel/Panel";
 const Root = () => {
+  const [user, SetUser] = useState(false);
+
   return (
     <div>
       <Header />
-      <div className="inner_box">
-        {/*  inner-box 에 있는 컴포넌트 추가하시면 됩니다.  */}
-      </div>
+      <Route exact path="/" component={Main} />
+      <Route path="/faq" render={() => <Faq user={user} />} />
+      <Route path="/guide" render={() => <Guide user={user} />} />
+      <Route path="/login" component={Login} />
+      <Route path="/terms" component={Terms} />
+      <Route path="/panel" component={Panel} />
       <Footer />
     </div>
   );
