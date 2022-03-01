311a9be4eb5838516bed26c40d5aca623ba3d8d6
Jang Woo Bin <jwb449730@naver.com>
2022-03-01 19:31:39 +0900

    Add scrollToTop, update index

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./util/ScrollToTop";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
