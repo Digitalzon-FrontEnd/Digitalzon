febfd83e50b3829cab15424b8f292ee80758f4f9
june0-K <ts1revan@naver.com>
2022-03-03 18:27:03 +0900

    update index, Header, Footer for adjust header and footer CSS

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <header>
-      <div className="header-box">
+      <div className="header-inner">
         <Link to="/">
           <div className="logo">
             <img
