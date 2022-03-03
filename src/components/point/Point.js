6a7fee4de74546dba05b6cfb2420f1127bcb6c07
WhiteMIron <bmc131@daum.net>
2022-03-03 15:36:01 +0900

    add _redirects

---
+++ src/components/point/Point.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Point = () => {
+  return (
+    <div className="inner">
+      <div className="point-inner">abc</div>
+    </div>
+  );
+};
+
+export default Point;
