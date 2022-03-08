bb5adb47700c547fd0b3dddab2556e364cda7904
june0-K <ts1revan@naver.com>
2022-03-08 21:42:37 +0900

    udate SurveyRegist, checkbox version

--- src/components/survey/SurveyRegist.js
+++ src/components/survey/SurveyRegist.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./SurveyRegist.css";
 
 function SurveyRegist ({ modalClose }) {
-    const [checkSex,setCheckSex] = useState(false);
-    const changeCheckSex = (e) => {
-        setCheckSex(e.target.checked);
+    const fileInput = useRef();
+    const fileName = useRef();
+    const panelNum = useRef();
+    const movefileName = () => {
+        fileName.current.value = fileInput.current.value;
+        panelNum.current.innerText = `8,545건`
     }
+    const [checkSex, setCheckSex] = useState(false);
+    const [checkAge, setCheckAge] = useState(false); 
+    const [checkMobileOperator, setCheckMobileOperator] = useState(false);
+    const [checkElectricDevice, setCheckElectricDevice] = useState(false);
     return(
         <div className="surveyRg">
             <div className="svRg-pageTitle-box">
@@ -30,9 +37,10 @@ function SurveyRegist ({ modalClose }) {
                     </li>
                     <li className="svRg-form-list">
                         <label htmlFor="svRgFileName">방송패널 등록</label>
-                        <input id="svRgFileRegister" type="file" />
+                        <input id="svRgFileRegister" type="file" ref={fileInput} onChange={movefileName} />
                         <label id="svRgFileBtn" className="btn-s btn-o" htmlFor="svRgFileRegister">파일 등록</label>
-                        <input id="svRgFileName" readOnly />
+                        <input id="svRgFileName" ref={fileName} readOnly />
+                        <p id="svRGPanelNum" ref={panelNum}></p>
                     </li>
                     <li className="svRg-form-list">
                         <label htmlFor="svRgDateFrom">요청기간</label>
@@ -46,7 +54,7 @@ function SurveyRegist ({ modalClose }) {
                             <li className="svRg-profile-list">
                                 <h6 className="svRg-profile-steps">1단계</h6>
                                 <dl className="svRg-options-box">
-                                    <dt className="svRg-options-depth1"><input type="checkbox" name="sex" onChange={changeCheckSex}/>성별</dt>
+                                    <dt className="svRg-options-depth1"><input type="checkbox" onChange={e => setCheckSex(e.target.checked)}/>성별</dt>
                                     {checkSex ?
                                     <dd>
                                         <select>
@@ -56,36 +64,50 @@ function SurveyRegist ({ modalClose }) {
                                     </dd> : null }
                                 </dl>
                                 <dl className="svRg-options-box">
-                                    <dt className="svRg-options-depth1"><input type="checkbox" name="age" />나이</dt>
-                                    <dd>
-                                        <input type="range" />
-                                    </dd>
+                                    <dt className="svRg-options-depth1"><input type="checkbox" onChange={e => setCheckAge(e.target.checked)} />나이</dt>
+                                    {checkAge ?
+                                    <dd id="svRgOptionDepth2Age">
+                                        <input id="svRgInputMinage" type="number" min={0} max={100} defaultValue="" />
+                                        <label htmlFor="svRgInputMinage">세 이상</label>
+                                        <input id="svRgInputMaxage" type="number" min={0} max={100} defaultValue="" />
+                                        <label htmlFor="svRgInputMaxage">세 이하</label>
+                                    </dd> : null }
                                 </dl>
                             </li>
                             <li className="svRg-profile-list">
                                 <h6 className="svRg-profile-steps">2단계</h6>
                                 <dl className="svRg-options-box">
-                                    <dt className="svRg-options-depth1"><input type="checkbox" />통신사</dt>
+                                    <dt className="svRg-options-depth1"><input type="checkbox" onChange={e => setCheckMobileOperator(e.target.checked)} />통신사</dt>
+                                    {checkMobileOperator ?
                                     <dd>
                                         <select>
                                             <option value="SKT">SKT</option>
                                             <option value="KT">KT</option>
                                             <option value="LG U+">LG U+</option>
                                         </select>
-                                    </dd>
+                                    </dd> : null }
                                 </dl>
                             </li>
                             <li className="svRg-profile-list">
                                 <h6 className="svRg-profile-steps">3단계</h6>
                                 <dl className="svRg-options-box">
-                                    <dt className="svRg-options-depth1"><input type="checkbox" />전자기기</dt>
+                                    <dt className="svRg-options-depth1"><input type="checkbox" onChange={e => setCheckElectricDevice(e.target.checked)} />전자기기</dt>
+                                    { checkElectricDevice ?
                                     <dd>
                                         <select>
-                                            <option value="SKT">SKT</option>
-                                            <option value="KT">KT</option>
-                                            <option value="LG U+">LG U+</option>
+                                            <optgroup label="휴대폰">
+                                                <option value="갤럭시S20">갤럭시S20</option>
+                                                <option value="갤럭시S21">갤럭시S21</option>
+                                                <option value="아이폰12">아이폰12</option>
+                                                <option value="아이폰13">아이폰13</option>
+                                            </optgroup>
+                                            <optgroup label="노트북">
+                                                <option value="맥북프로">맥북 프로</option>
+                                                <option value="삼성울트라">삼성 갤럭시북 프로</option>
+                                                <option value="LG 그램">LG 그램 2022</option>
+                                            </optgroup>
                                         </select>
-                                    </dd>
+                                    </dd> : null }
                                 </dl>
                             </li>
                         </ul>
