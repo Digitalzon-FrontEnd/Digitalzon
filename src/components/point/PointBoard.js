773fba0a098f1658615088ac07363e942a956d4f
WhiteMIron <bmc131@daum.net>
2022-03-15 16:00:09 +0900

    update pointBoard - postsPerpage

--- src/components/point/PointBoard.js
+++ src/components/point/PointBoard.js
@@ -9,7 +9,7 @@ const PointBoard = ({ pointItems, setSelectPointItem }) => {
   };
 
   const [currentPage, setCurrentPage] = useState(1); //현재 페이지
-  const postsPerPage = 5; //한 페이지에 글 갯수
+  const postsPerPage = 10; //한 페이지에 글 갯수
 
   const indexOfLast = currentPage * postsPerPage; // 페이지를 글 갯수만큼 곱해서 보여준게 마지막 페이지넘버
   const indexOfFirst = indexOfLast - postsPerPage; // 마지막페이지 넘버 - 한 페이지의 글 갯수 = 첫번째 페이지 넘버
