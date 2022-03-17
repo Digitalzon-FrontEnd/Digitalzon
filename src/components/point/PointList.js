e2f131d9637241e8a701662d61becbdbfcb45489
WhiteMIron <bmc131@daum.net>
2022-03-17 09:41:40 +0900

    update Point-table fiexed width

--- src/components/point/PointList.js
+++ src/components/point/PointList.js
@@ -12,16 +12,16 @@ const PointList = ({
     // <Link to={`/point/view/${selectPointItem.id}`}>
     <table className="point-table">
       <tr>
-        <th>번호</th>
-        <th>업체명</th>
-        <th>연락처</th>
-        <th>이메일</th>
-        <th>포인트</th>
-        <th>구분</th>
-        <th>신청일</th>
-        <th>상태변경일</th>
-        <th>상태</th>
-        <th>상태변경자</th>
+        <th width="4%">번호</th>
+        <th width="5%">업체명</th>
+        <th width="10%">연락처</th>
+        <th width="10%">이메일</th>
+        <th width="5%">포인트</th>
+        <th width="5%">구분</th>
+        <th width="5%">신청일</th>
+        <th width="5%">상태변경일</th>
+        <th width="5%">상태</th>
+        <th width="5%">상태변경자</th>
       </tr>
       {pointItems.map((item) => {
         return (
